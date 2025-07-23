2eb36aaa0a26f026a2fa8d5d483d68e7b2a176a4
xsf <fei523688393@hotmail.com>
2025-07-23 22:48:45 +0800

    🎉 init: to nextjs

--- content/index.md
+++
@@ -1,3 +0,0 @@
-# This is a Content file for the Nuxt application.
-
-# It is used to define the structure and content of the page.
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen bg-gray-800 flex flex-col items-center justify-center text-2xl text-gray-400"> It's how I start...</div>
+
+  );
+}
