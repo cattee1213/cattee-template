d1504eb94ed408ea6f394054dff1e7c8aba7271b
xiaoshangfei <fei523688393@hotmail.com>
2025-07-24 14:27:46 +0800

    🐞 fix:

--- src/app/dashborad/demo/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function DemoPage() {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold text-slate-400">It is how I start...</h1>
-    </div>
-  );
-}
